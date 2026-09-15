e6a71569d4d25397cbf3b323fd14056fed639d20
shadow <shakhgamer063@gmail.com>
2026-09-15 14:29:52 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-base: '/buxoro_med_academy/',
+base: '/',
   plugins: [react()],
   server: {
     host: true, // telefondan bir tarmoq orqali ochish uchun
